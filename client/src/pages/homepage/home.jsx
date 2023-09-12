b835bb25a73f89a39a5a8614c098cdf2d569861d
Able Abenaitwe <ableabenaitwe@gmail.com>
2023-09-12 19:56:23 +0300

    updates

--- client/src/pages/homepage/home.jsx
+++ client/src/pages/homepage/home.jsx
@@ -1,21 +1,24 @@
-import Navbar from '../../components/navbar/navbar';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import './home.scss';
 
 const Home = () => {
-  return (
-    <>
-      <div className='header'>
-        <div>
-          <Navbar />
-        </div>
-      </div>
-      <div className='main'>
-        <div>editors pick</div>
-        <div>Trending</div>
-      </div>
-      <div className='footer'></div>
-    </>
-  );
+  const [trends, setTrends] = useState([]);
+  const [topPicks, setTopPicks] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3001').then((res) => {
+      setTrends(res);
+    });
+    axios.get('http://localhost:3001').then((res) => {
+      setTopPicks(res);
+    });
+  }, []);
+
+  console.log(trends);
+  console.log(topPicks);
+  return <h1>welcome</h1>;
 };
 
 export default Home;
