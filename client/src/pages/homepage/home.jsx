0915b43221086094e0d73686fc332545fdc39bc5
Able Abenaitwe <ableabenaitwe@gmail.com>
2023-09-12 22:51:20 +0300

    update

--- client/src/pages/homepage/home.jsx
+++ client/src/pages/homepage/home.jsx
@@ -1,24 +1,64 @@
-import { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import { useState, useEffect } from 'react';
 import './home.scss';
 
 const Home = () => {
-  const [trends, setTrends] = useState([]);
-  const [topPicks, setTopPicks] = useState([]);
+  const [trending, setTrending] = useState([]);
+  const [top, setTop] = useState([]);
+  const [recommended, setRecommeded] = useState({});
 
   useEffect(() => {
-    axios.get('http://localhost:3001').then((res) => {
-      setTrends(res);
-    });
-    axios.get('http://localhost:3001').then((res) => {
-      setTopPicks(res);
-    });
+    const fetchData = async () => {
+      const topRes = await axios.get('http://localhost:3001/blog/top');
+      const trendingRes = await axios.get(
+        'http://localhost:3001/blog/trending'
+      );
+      const recommendedRes = await axios.get(
+        'http://localhost:3001/blog/recommended'
+      );
+
+      setTop(topRes.data);
+      setTrending(trendingRes.data);
+      setRecommeded(recommendedRes.data);
+    };
+    fetchData();
   }, []);
 
-  console.log(trends);
-  console.log(topPicks);
-  return <h1>welcome</h1>;
-};
+  console.log(trending);
+  console.log(recommended);
+  console.log(top);
+  return (
+    <div>
+      {recommended && (
+        <div>
+          {/* Render Recommended Post */}
+          <h2>Recommended Post</h2>
+          <h3>{recommended.title}</h3>
+          <p>{recommended.content}</p>
+        </div>
+      )}
 
+      <div>
+        {trending.map((post) => (
+          <div key={post.id}>
+            {/* Render Trending Post */}
+            <h3>{post.title}</h3>
+            <p>{post.content}</p>
+          </div>
+        ))}
+      </div>
+
+      <div className='top'>
+        {/* Render Top Posts */}
+        <h2>Top Posts</h2>
+        {top.map((post) => (
+          <div key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.content}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 export default Home;
